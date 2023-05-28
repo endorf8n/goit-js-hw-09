4dc42a3769605285b3aaeb05fe0f30b5297ae72a
endorf8n <128092594+endorf8n@users.noreply.github.com>
2023-05-28 21:50:39 +0300

    add libraries and function

---
+++ src/js/02-timer.js
@@ -0,0 +1,15 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import convertMs from './convertMs';
+
+let refs = {
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+  input: document.querySelector('#datetime-picker'),
+  button: document.querySelector('[data-start]'),
+};
+
+let timerId = null;
