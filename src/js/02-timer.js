6d70a510ae030fc9a13e1a0ce4cbadb55a6fe08d
endorf8n <128092594+endorf8n@users.noreply.github.com>
2023-05-30 18:30:38 +0300

    task-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,7 +3,9 @@ import 'flatpickr/dist/flatpickr.min.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import convertMs from './convertMs';
 
-let refs = {
+let intervalId = null;
+
+const refs = {
   days: document.querySelector('[data-days]'),
   hours: document.querySelector('[data-hours]'),
   minutes: document.querySelector('[data-minutes]'),
@@ -12,16 +14,47 @@ let refs = {
   button: document.querySelector('[data-start]'),
 };
 
-const options = {
+refs.button.disabled = true;
+
+flatpickr(refs.input, {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0] <= Date.now()) {
+      Notify.failure('Please choose a date in the future');
+    } else {
+      refs.button.disabled = false;
+    }
   },
-};
+});
 
-let timerId = null;
+function onStartClick() {
+  intervalId = setInterval(() => {
+    const selectedDate = new Date(refs.input.value);
 
-flatpickr(refs.input, options);
+    const diff = selectedDate - Date.now();
+
+    if (diff <= 0) {
+      stop();
+      return;
+    }
+    let { days, hours, minutes, seconds } = convertMs(diff);
+
+    refs.days.textContent = addLeadingZero(days);
+    refs.hours.textContent = addLeadingZero(hours);
+    refs.minutes.textContent = addLeadingZero(minutes);
+    refs.seconds.textContent = addLeadingZero(seconds);
+  }, 1000);
+}
+
+function stop() {
+  clearInterval(intervalId);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+refs.button.addEventListener('click', onStartClick);
